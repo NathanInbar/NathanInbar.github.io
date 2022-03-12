3191fac7d7b3a5592b86efd4383fb000971c1559
Nathan Inbar <nathaninbar@gmail.com>
2022-03-11 17:13:51 -0800

    main container (header) makeover

--- portfolio-site/src/App.svelte
+++ portfolio-site/src/App.svelte
@@ -5,8 +5,15 @@
 
 <span id="bkg">
   <div id="main__container">
-    <h1>Hello World</h1>
+    <h1>Nathan Inbar</h1>
+    <h4>Student 👨‍🎓, Computer Scientist 👨‍💻, Photographer 📷</h4>
+    <p>
+      Hey, I'm Nathan. I've developed and published a mobile game, 2x winning
+      hackathon entry, and many other projects. Love to learn and create awesome
+      applications.
+    </p>
   </div>
+  <br />
   <Test /><Test /><Test /><Test /><Test /><Test /><Test /><Test /><Test /><Test
   /><Test /><Test /><Test /><Test />
   <Test /><Test /><Test /><Test /><Test /><Test /><Test /><Test /><Test /><Test
@@ -24,18 +31,23 @@
 
   #main__container {
     display: flex;
+    flex-direction: column;
     border: 1px solid magenta;
-    background-color: rgba(
-      rgb(196, 53, 53),
-      rgb(28, 199, 28),
-      rgb(37, 37, 151),
-      0.3
-    );
     margin-left: var(--global_margin);
-    margin-right: var(--global_margin);
   }
-
+  /* centers main container on mobile */
+  @media only screen and (max-width: 620px) {
+    #main__container {
+      margin-right: var(--global_margin);
+    }
+  }
   h1 {
+    font-size: 4rem;
+    color: var(--global_header_light);
+    margin-bottom: 0;
+  }
+  h4 {
+    margin-top: 0;
     color: var(--global_header_light);
   }
 </style>
