6352b43d66e446dd248291de8def16b005f9428e
Nathan Inbar <nathaninbar@gmail.com>
2022-03-11 17:52:12 -0800

    Added Content Containers + css

--- portfolio-site/src/App.svelte
+++ portfolio-site/src/App.svelte
@@ -1,39 +1,52 @@
 <script>
+  import ContentContainer from "./Components/ContentContainer.svelte";
   import Test from "./Components/Test.svelte";
-  let x = 20;
 </script>
 
 <span id="bkg">
-  <div id="main__container">
-    <h1>Nathan Inbar</h1>
-    <h4>Student 👨‍🎓, Computer Scientist 👨‍💻, Photographer 📷</h4>
-    <p>
-      Hey, I'm Nathan. I've developed and published a mobile game, 2x winning
-      hackathon entry, and many other projects. Love to learn and create awesome
-      applications.
-    </p>
-  </div>
-  <br />
-  <Test /><Test /><Test /><Test /><Test /><Test /><Test /><Test /><Test /><Test
-  /><Test /><Test /><Test /><Test />
-  <Test /><Test /><Test /><Test /><Test /><Test /><Test /><Test /><Test /><Test
-  /><Test /><Test /><Test /><Test />
+  <span id="content__bkg">
+    <div id="main__container">
+      <h1>Nathan Inbar</h1>
+      <h4>Student 👨‍🎓, Computer Scientist 👨‍💻, Photographer 📷</h4>
+      <p>
+        Hey, I'm Nathan. I've developed and published a mobile game, 2x winning
+        hackathon entry, and many other projects. Love to learn and create
+        awesome applications.
+      </p>
+    </div>
+    <br />
+
+    <ContentContainer title="Projects" />
+    <ContentContainer title="Photos" />
+
+    <Test /><Test /><Test /><Test /><Test /><Test /><Test /><Test /><Test
+    /><Test /><Test /><Test /><Test /><Test />
+    <Test /><Test /><Test /><Test /><Test /><Test /><Test /><Test /><Test
+    /><Test /><Test /><Test /><Test /><Test />
+  </span>
 </span>
 
 <style>
   #bkg {
-    /*background overlay currently disabled */
-    background-color: rgba(0, 0, 0, 0);
+    /*full background overlay currently disabled */
+    background-color: rgb(242, 242, 255, 0);
     width: 100%;
     height: 100%;
     position: absolute;
+  }
+  #content__bkg {
+    /* content container background rgb(242, 242, 255, ); */
+    position: absolute;
+    width: 80%;
+    height: 100%;
+    margin-left: var(--global_margin);
+    background-color: rgba(242, 242, 255, 0.6);
   }
 
   #main__container {
     display: flex;
     flex-direction: column;
-    border: 1px solid magenta;
-    margin-left: var(--global_margin);
+    border: 0px solid magenta;
   }
   /* centers main container on mobile */
   @media only screen and (max-width: 620px) {
@@ -45,6 +58,7 @@
     font-size: 4rem;
     color: var(--global_header_light);
     margin-bottom: 0;
+    margin-left: -4px;
   }
   h4 {
     margin-top: 0;
---
+++ portfolio-site/src/Components/ContentContainer.svelte
@@ -0,0 +1,26 @@
+<script>
+  export let title;
+</script>
+
+<div>
+  <h1><span id="dropdown__icon">>></span> {title}</h1>
+</div>
+
+<style>
+  div {
+    margin-left: 0;
+  }
+  #dropdown__icon {
+    display: inline-block;
+  }
+  #dropdown__icon:hover {
+    animation: rotate-rt 0.5s 1;
+    animation-fill-mode: forwards;
+  }
+
+  @keyframes rotate-rt {
+    100% {
+      transform: rotate(90deg);
+    }
+  }
+</style>
